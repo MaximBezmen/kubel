652772ea0c2b0b5776d6a05813c50f5cbc3cb126
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-02-10 10:22:16 +0300

    Add crate ad

--- src/main/java/com/kubel/service/impl/AdServiceImpl.java
+++ src/main/java/com/kubel/service/impl/AdServiceImpl.java
@@ -1,6 +1,9 @@
 package com.kubel.service.impl;
 
+import com.kubel.entity.Account;
 import com.kubel.entity.Ad;
+import com.kubel.exception.ResourceNotFoundException;
+import com.kubel.repo.AccountRepository;
 import com.kubel.repo.AdRepository;
 import com.kubel.service.AdService;
 import com.kubel.service.dto.AdDto;
@@ -12,15 +15,20 @@ public class AdServiceImpl implements AdService {
 
     private final AdMapper adMapper;
     private final AdRepository adRepository;
+    private final AccountRepository accountRepository;
 
-    public AdServiceImpl(AdMapper adMapper, AdRepository adRepository) {
+    public AdServiceImpl(AdMapper adMapper, AdRepository adRepository, AccountRepository accountRepository) {
         this.adMapper = adMapper;
         this.adRepository = adRepository;
+        this.accountRepository = accountRepository;
     }
 
     @Override
     public AdDto crateAdByUserId(Long id, AdDto dto) {
         Ad adEntity = adMapper.toEntity(dto);
+        Account accountEntity = accountRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "id", id));
+        adEntity.setAccount(accountEntity);
         return adMapper.toDto(adRepository.save(adEntity));
     }
 }
