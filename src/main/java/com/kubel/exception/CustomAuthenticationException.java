dc0491de5d40adbb623a17f780bd8a16f61ff255
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-03-05 14:01:54 +0300

    Add massage in exception

--- src/main/java/com/kubel/exception/CustomAuthenticationException.java
+++ src/main/java/com/kubel/exception/CustomAuthenticationException.java
@@ -1,13 +1,16 @@
 package com.kubel.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class CustomAuthenticationException extends AuthenticationException {
     public CustomAuthenticationException(String msg, Throwable cause) {
         super(msg, cause);
     }
 
-    public CustomAuthenticationException(String msg) {
-        super(msg);
+    public CustomAuthenticationException() {
+        super("Unauthorized");
     }
 }
--- src/main/java/com/kubel/security/JWTAuthenticationFilter.java
+++ src/main/java/com/kubel/security/JWTAuthenticationFilter.java
@@ -3,13 +3,16 @@ package com.kubel.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kubel.exception.Forbidden;
+import com.kubel.exception.CustomAuthenticationException;
 import com.kubel.service.dto.AccountDto;
 import lombok.SneakyThrows;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.NullRememberMeServices;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -23,7 +26,9 @@ import java.util.Date;
 import static com.kubel.security.SecurityConstants.*;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
+    private AuthenticationFailureHandler failureHandler;
+    private RememberMeServices rememberMeServices;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -32,16 +37,15 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @SneakyThrows
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-            {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
         try {
             AccountDto accountDto = new ObjectMapper().readValue(request.getInputStream(), AccountDto.class);
             return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                     accountDto.getLogin(),
                     accountDto.getPassword(),
                     new ArrayList<>()));
-        } catch (Forbidden e) {
-            throw new Forbidden();
+        } catch (IOException e) {
+            throw new CustomAuthenticationException();
         }
     }
 
@@ -61,6 +65,14 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-       new AuthenticationFailureHandler().onAuthenticationFailure(request,response,failed);
+        SecurityContextHolder.clearContext();
+        this.failureHandler = new AuthenticationFailureHandler();
+        this.rememberMeServices = new NullRememberMeServices();
+        this.logger.trace("Failed to process authentication request", failed);
+        this.logger.trace("Cleared SecurityContextHolder");
+        this.logger.trace("Handling authentication failure");
+        this.rememberMeServices.loginFail(request, response);
+        this.failureHandler.onAuthenticationFailure(request, response, failed);
+
     }
 }
