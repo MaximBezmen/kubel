9ca60687b82554039701dbd26fd9d487f58022eb
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-03-06 22:46:40 +0300

    Add page

--- src/main/java/com/kubel/KubelBackApplication.java
+++ src/main/java/com/kubel/KubelBackApplication.java
@@ -1,6 +1,7 @@
 package com.kubel;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springdoc.core.SpringDocUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class KubelBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(KubelBackApplication.class, args);
+        SpringDocUtils.getConfig().replaceWithClass(org.springframework.data.domain.Pageable.class,
+                org.springdoc.core.converters.models.Pageable.class);
     }
 
 }
