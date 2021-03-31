e0aea2b5112165acac14b0975da2de23650b48ac
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-03-31 22:00:14 +0300

    Add get all ad by user id

--- src/main/java/com/kubel/security/SecurityConstants.java
+++ src/main/java/com/kubel/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.kubel.security;
 
 public class SecurityConstants {
     public static final String SECRET = "qwebnbndbasdanmaxbezmen";
-    public static final long EXPIRATION_TIME = 1000_000;// 15 min
+    public static final long EXPIRATION_TIME = 3000_000;// 15 min
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SING_UP_URL = "/users/registration";
