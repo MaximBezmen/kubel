d087c86de972bad0dfb44af9b34ebea7375d4f8f
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-04-04 18:27:18 +0300

    Add  fix oauth

--- src/main/java/com/kubel/security/JWTAuthenticationFilter.java
+++ src/main/java/com/kubel/security/JWTAuthenticationFilter.java
@@ -8,12 +8,8 @@ import lombok.SneakyThrows;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.NullRememberMeServices;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
