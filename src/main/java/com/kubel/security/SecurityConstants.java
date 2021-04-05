54df2f994d6c784ed88c43184ef989479500a647
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-04-05 22:59:41 +0300

    Add  fix oauth

--- src/main/java/com/kubel/security/SecurityConstants.java
+++ src/main/java/com/kubel/security/SecurityConstants.java
@@ -1,8 +1,11 @@
 package com.kubel.security;
 
 public class SecurityConstants {
+    private SecurityConstants(){
+
+    }
     public static final String SECRET = "qwebnbndbasdanmaxbezmen";
-    public static final long EXPIRATION_TIME = 864000000;// 15 min
+    public static final long EXPIRATION_TIME = 864000000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SING_UP_URL = "/users/registration";
